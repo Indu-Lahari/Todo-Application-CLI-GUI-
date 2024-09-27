a3282de3b371f4e7f5e44be39519b81b6dd22b91
Indu <indulahari6@gmail.com>
2024-09-27 19:17:28 +0530

    Using List Comprehension to remove break lines

--- main.py
+++ main.py
@@ -21,12 +21,7 @@ while True:
             todos = file.readlines()
             file.close()
 
-            new_todos = []
-
-            for item in todos:
-                new_item = item.strip('\n')
-                new_todos.append(new_item)
-            print(todos)
+            new_todos = [item.strip('\n') for item in todos]
 
             for index, item in enumerate(new_todos):
                 row = f"{index + 1}.{item}"
