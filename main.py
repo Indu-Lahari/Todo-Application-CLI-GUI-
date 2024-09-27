dca742b7930fa40fca0e50dfd3d9d8c292e167d4
Indu <indulahari6@gmail.com>
2024-09-27 14:24:29 +0530

    Improving the Program (for-loop & strip)

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ todos = []
 
 while True:
     user_action = input("Type add, show or exit: ")
+    user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
         case 'show':
-            print(todos)
+            for item in todos:
+                print(item)
         case 'exit':
             break
+
+print("Bye!")
