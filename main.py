76e279a6ce09e64e2c8b59dffd4c578fa2e8f99d
Indu <indulahari6@gmail.com>
2024-09-27 13:10:48 +0530

    Storing user input

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-user_prompt = input("Enter a todo:")
-print(user_prompt)
+user_prompt = "Enter a todo:"
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+todo3 = input(user_prompt)
+
+todos = [todo1, todo2, todo3]
+print(todos)
