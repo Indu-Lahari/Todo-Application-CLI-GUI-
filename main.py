f08bb666ef57a9139ec0107f695c5927820c6ac9
Indu <indulahari6@gmail.com>
2024-09-27 20:00:14 +0530

    Optimizing edit and complete (context manager)

--- main.py
+++ main.py
@@ -29,12 +29,26 @@ while True:
         case 'edit':
             number = int(input("Number of the todo to edit: "))
             number = number - 1
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             new_todo = input("Enter new todo: ")
-            todos[number] = new_todo
+            todos[number] = new_todo + '\n'
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case 'complete':
             number = int(input("Number of the todo i.e; completed: "))
+
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+
             todos.pop(number - 1)
+
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
 
         case 'exit':
             break
