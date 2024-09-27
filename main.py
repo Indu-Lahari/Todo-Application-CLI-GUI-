455887c1a4656eb594ca5caedb962041cf2eac07
Indu <indulahari6@gmail.com>
2024-09-27 13:41:46 +0530

    Getting user input repeatedly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 user_prompt = "Enter a todo:"
-todo1 = input(user_prompt)
-todo2 = input(user_prompt)
-todo3 = input(user_prompt)
 
-todos = [todo1, todo2, todo3]
-print(todos)
+while True:
+    todo = input(user_prompt)
+    print(todo)
+    print("Next...")
