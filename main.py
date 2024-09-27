ff1aebd98aef1c6341169829d7327be5e309d9de
Indu <indulahari6@gmail.com>
2024-09-27 14:57:56 +0530

    'Edit' Feature (List indexing & Type Conversion)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, display or exit: ")
+    user_action = input("Type add, show, display, edit or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -12,6 +12,11 @@ while True:
             for item in todos:
                 item = item.title()
                 print(item)
+        case 'edit':
+            number = int(input("Numer of the todo to edit: "))
+            number = number - 1
+            new_todo = input("Enter new todo: ")
+            todos[number] = new_todo
         case 'exit':
             break
         case _:
