7fbe94bc3174889a2f3a8b3c7ba462fb9b437161
Indu <indulahari6@gmail.com>
2024-09-27 13:46:49 +0530

    Storing user input repeatedly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 user_prompt = "Enter a todo:"
 
+todos = []
+
 while True:
     todo = input(user_prompt)
-    print(todo)
-    print("Next...")
+    print(todo.capitalize())
+    todos.append(todo)
+    print(todos)
