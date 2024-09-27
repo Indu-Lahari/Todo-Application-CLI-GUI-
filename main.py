af8a9fc6b7d5e83b89a855e8e9dfcc8ac11828e3
Indu <indulahari6@gmail.com>
2024-09-27 19:14:12 +0530

    Using for loop removing break lines

--- main.py
+++ main.py
@@ -21,7 +21,14 @@ while True:
             todos = file.readlines()
             file.close()
 
-            for index, item in enumerate(todos):
+            new_todos = []
+
+            for item in todos:
+                new_item = item.strip('\n')
+                new_todos.append(new_item)
+            print(todos)
+
+            for index, item in enumerate(new_todos):
                 row = f"{index + 1}.{item}"
                 print(row)
 
