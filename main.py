f4ecd4b7d398adc53cbd277e8231b4eaf7b545c0
Indu <indulahari6@gmail.com>
2024-09-27 14:29:16 +0530

    Coding Experiments (Bitwise OR, title, case_)

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show or exit: ")
+    user_action = input("Type add, show, display or exit: ")
     user_action = user_action.strip()
 
     match user_action:
         case 'add':
             todo = input("Enter a todo: ")
             todos.append(todo)
-        case 'show':
+        case 'show' | 'display':
             for item in todos:
+                item = item.title()
                 print(item)
         case 'exit':
             break
+        case _:
+            print("Hey, you entered an unknown command")
 
 print("Bye!")
