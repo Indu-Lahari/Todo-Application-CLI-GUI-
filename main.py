54a2db997fe3eb2f9838a3e29611ba6294190d09
Indu <indulahari6@gmail.com>
2024-09-27 17:11:34 +0530

    'Complete' Feature

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 todos = []
 
 while True:
-    user_action = input("Type add, show, display, edit or exit: ")
+    user_action = input("Type add, show/display, edit, complete or exit: ")
     user_action = user_action.strip()
 
     match user_action:
@@ -10,13 +10,16 @@ while True:
             todos.append(todo)
         case 'show' | 'display':
             for index, item in enumerate(todos):
-                row = f"{index}.{item}"
+                row = f"{index + 1}.{item}"
                 print(row)
         case 'edit':
-            number = int(input("Numer of the todo to edit: "))
+            number = int(input("Number of the todo to edit: "))
             number = number - 1
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo
+        case 'complete':
+            number = int(input("Number of the todo i.e; completed: "))
+            todos.pop(number - 1)
         case 'exit':
             break
         case _:
