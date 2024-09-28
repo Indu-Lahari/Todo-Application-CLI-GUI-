d3d7fe32fc275041836fd7fbca3b602ce29e6633
Indu <indulahari6@gmail.com>
2024-09-28 14:00:37 +0530

    Organizing code in modules(Local Modules)

---
+++ functions.py
@@ -0,0 +1,11 @@
+# Parameter in function
+def get_todos(filepath='todos.txt'):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath='todos.txt'):
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+
--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-# Parameter in function
-def get_todos(filepath='todos.txt'):
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath='todos.txt'):
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos_arg)
-
+from functions import get_todos, write_todos
+# import functions
 
 while True:
     user_action = input("Type add, show/display, edit, complete or exit: ")
@@ -20,6 +11,7 @@ while True:
 
         # Argument in function calls
         todos = get_todos()
+        # todos = functions.get_todos()
 
         todos.append(todo + '\n')
 
