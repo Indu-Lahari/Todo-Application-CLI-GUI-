7be60f91651e95f94f6537e6b2427aa44ac72d0d
Indu <indulahari6@gmail.com>
2024-09-28 13:35:20 +0530

    Utilizing Default Arguments

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Parameter in function
-def get_todos(filepath):
+def get_todos(filepath='todos.txt'):
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath='todos.txt'):
     with open(filepath, 'w') as file_local:
         file_local.writelines(todos_arg)
 
@@ -19,15 +19,15 @@ while True:
         todo = user_action[4:]
 
         # Argument in function calls
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         todos.append(todo + '\n')
 
-        write_todos('todos.txt', todos)
+        write_todos(todos)
 
 
     elif user_action.startswith('show'):
-        todos = get_todos('todos.txt')
+        todos = get_todos()
 
         # List Comprehension
         # new_todos = [item.strip('\n') for item in todos]
@@ -42,12 +42,12 @@ while True:
             number = int(user_action[5:])
             number = number - 1
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             new_todo = input("Enter new todo: ")
             todos[number] = new_todo + '\n'
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
         except ValueError:
             print("Your command is not valid! Please enter the todo number")
@@ -57,13 +57,13 @@ while True:
         try:
             number = int(user_action[9:])
 
-            todos = get_todos('todos.txt')
+            todos = get_todos()
 
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
 
-            write_todos('todos.txt', todos)
+            write_todos(todos)
 
             message = f"Todo {todo_to_remove} was removed from the list."
             print(message)
